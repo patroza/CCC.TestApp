efe1f92e2e05516ecd07a779b9b8c690cfcb050d
Patrick Roza <roza@sixnetworks.net>
2013-12-15 22:41:56 +0100

    Added Projects: Added UI.Desktop skeleton project Added UI.Desktop.DR and UI.Web.DR projects. To keep Dependency Resolution separate from UI concerns.

---
+++ src/CCC.TestApp.UI.Desktop/Views/ShellView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace CCC.TestApp.UI.Desktop.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
