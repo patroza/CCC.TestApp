cf1ebeb62d14f0574f0a249dcc4ccdf3b5d99bc2
Patrick Roza <roza@sixnetworks.net>
2013-12-15 22:49:55 +0100

    Circular dependency lunacy.. Separate DR projects sounded great in theory, but are technically hard to achieve..

---
+++ src/CCC.TestApp.UI.Desktop.DR/AppBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using CCC.TestApp.UI.Desktop.ViewModels;
+
+namespace CCC.TestApp.UI.Desktop.DR
+{
+    public class AppBootstrapper : Bootstrapper<ShellViewModel>
+    {
+    }
+}
--- src/CCC.TestApp.UI.Desktop/ViewModels/ShellViewModel.cs
+++ src/CCC.TestApp.UI.Desktop/ViewModels/ShellViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CCC.TestApp.UI.Desktop.ViewModels
 {
-    class ShellViewModel
+    public class ShellViewModel
     {
     }
 }
