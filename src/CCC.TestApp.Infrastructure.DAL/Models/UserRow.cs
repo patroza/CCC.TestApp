ac589e3ef6ce2f96d5b7be6e2e41820df4795053
Patrick Roza <roza@sixnetworks.net>
2013-12-16 15:49:56 +0100

    Made the DAL more DAL like; using it's own Model (row) layer

---
+++ src/CCC.TestApp.Infrastructure.DAL/Models/UserRow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CCC.TestApp.Infrastructure.DAL.Models
+{
+    [Table("Users")]
+    public class UserRow
+    {
+        [Column, Required]
+        public Guid Id { get; set; }
+
+        [Column]
+        public string Password { get; set; }
+
+        [Column, Required]
+        public string UserName { get; set; }
+    }
+}
--- src/CCC.TestApp.Infrastructure.DAL/Repositories/MockUserRepository.cs
+++ src/CCC.TestApp.Infrastructure.DAL/Repositories/MockUserRepository.cs
@@ -1,24 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using CCC.TestApp.Core.Application.DALInterfaces;
 using CCC.TestApp.Core.Domain.Entities;
+using CCC.TestApp.Infrastructure.DAL.Models;
 
 namespace CCC.TestApp.Infrastructure.DAL.Repositories
 {
     public class MockUserRepository : IUserRepository
     {
-        readonly Dictionary<Guid, User> _userList = new Dictionary<Guid, User>();
+        readonly Dictionary<Guid, UserRow> _userList = new Dictionary<Guid, UserRow>();
 
         public MockUserRepository() {
             var guid = Guid.NewGuid();
-            _userList.Add(guid, new User(guid) { UserName = "Test user 1" });
+            _userList.Add(guid, new UserRow {Id = guid, UserName = "Test user 1"});
             guid = Guid.NewGuid();
-            _userList.Add(guid, new User(guid) { UserName = "Test user 2" });
+            _userList.Add(guid, new UserRow {Id = guid, UserName = "Test user 2"});
         }
 
         public User Find(Guid userId) {
-            return _userList.ContainsKey(userId) ? _userList[userId] : null;
+            return _userList.ContainsKey(userId) ? Mapper.DynamicMap<User>(_userList[userId]) : null;
         }
 
         public void Update(User user) {
@@ -35,7 +37,7 @@ namespace CCC.TestApp.Infrastructure.DAL.Repositories
         }
 
         public List<User> All() {
-            return _userList.Values.ToList();
+            return _userList.Values.Select(Mapper.DynamicMap<User>).ToList();
         }
 
         public User Find(string userName) {
