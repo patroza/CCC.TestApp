a5e78e72d0d20ca9e9b84480b7919d11f632d2b3
Patrick Roza <roza@sixnetworks.net>
2013-12-16 12:33:06 +0100

    Improved UserView

--- src/CCC.TestApp.UI.Desktop/ViewModels/UserViewModel.cs
+++ src/CCC.TestApp.UI.Desktop/ViewModels/UserViewModel.cs
@@ -5,7 +5,10 @@ namespace CCC.TestApp.UI.Desktop.ViewModels
     public class UserViewModel : ScreenBase
     {
         public UserViewModel(User user) {
+            User = user;
             base.DisplayName = "Show User";
         }
+
+        public User User { get; private set; }
     }
 }
