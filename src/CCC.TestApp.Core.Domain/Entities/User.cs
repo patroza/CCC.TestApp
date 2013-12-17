bb4939e532ea6b31f2f4a4e2c647feac217454bf
Patrick Roza <roza@sixnetworks.net>
2013-12-17 11:16:41 +0100

    Dont require Guid in constructor of entity

--- src/CCC.TestApp.Core.Domain/Entities/User.cs
+++ src/CCC.TestApp.Core.Domain/Entities/User.cs
@@ -4,11 +4,7 @@ namespace CCC.TestApp.Core.Domain.Entities
 {
     public class User
     {
-        public User(Guid id) {
-            Id = id;
-        }
-
-        public Guid Id { get; private set; }
+        public Guid Id { get; set; }
         public string Password { get; set; }
         public string UserName { get; set; }
     }
